6c65c818f59a053a9a542ccc9d7dc3f832cf3eb0
shiwani <shiwanituladhar4@gmail.com>
2026-05-04 17:06:23 +0545

    hide

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://shiwani:NfakFHZ15WNsUxlK@cluster0.yublivi.mongodb.net/Notes");
+        await mongoose.connect("url");
         console.log("DB connected");
     }catch(err) {
         console.log("DB error", err);
