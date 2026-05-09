c224fef923d15193268d726c50ec38b98306bb5d
shiwani <shiwanituladhar4@gmail.com>
2026-05-09 18:00:56 +0545

    diff client

--- client/src/providers/public-route.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-function PublicRoute({ children }) {
-  const token = localStorage.getItem("token");
-  return token ? <Navigate to="/notes" replace /> : children;
-}
-
-export default PublicRoute
