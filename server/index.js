d94e4f876f08657680c33f998bcb89316fd3ddc4
shiwani <shiwanituladhar4@gmail.com>
2026-05-10 15:23:08 +0545

    deploy

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://meropatra.vercel.app"
+}));
 app.use(express.json());
 
 connectDB();
@@ -21,6 +23,8 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-app.listen(5000, () => {
-  console.log("Backend running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
 });
